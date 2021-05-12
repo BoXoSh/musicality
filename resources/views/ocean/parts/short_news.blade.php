1569ac0699a5ffe509b1208020097c05a6d24dd9
Bakhodir <boxo2016@gmail.com>
2021-05-12 23:51:17 +0500

    mp3 ocean

--- resources/views/ocean/parts/short_news.blade.php
+++ resources/views/ocean/parts/short_news.blade.php
@@ -3,8 +3,9 @@
         $xf = $post->xfields;
         $song_name = $xf->get(config('xfields.song_name'));
         $artist_name = $xf->get(config('xfields.artist_name'));
+        $song_poster = $xf->get(config('xfields.song_poster'));
     @endphp
-<div class="track-item fx-row fx-middle js-item" data-track="{{ $xf->get(config('mp3_url')) }}" data-title="{{ $song_name }}" data-artist="{{ $artist_name }}" data-img="{{ $xf->get(config('song_poster')) }}">
+<div class="track-item fx-row fx-middle js-item" data-track="{{ $xf->get(config('mp3_url')) }}" data-title="{{ $song_name }}" data-artist="{{ $artist_name }}" data-img="{{ $song_poster }}">
     <div class="track-play fx-col fx-center anim js-play"><span class="fas fa-play"></span></div>
     <a class="track-desc fx-1 nowrap" href="{{ route('home.get_post', ['id'=> $post->id, 'alt_name' => $post->alt_name]) }}">
         <div class="track-title">{{ $song_name }}</div>
