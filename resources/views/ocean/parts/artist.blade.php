ec9573faaf969f6c750825300254115ef2d51377
Bakhodir <boxo2016@gmail.com>
2021-05-13 12:52:41 +0500

    bootstrap grid

--- resources/views/ocean/parts/artist.blade.php
+++ resources/views/ocean/parts/artist.blade.php
@@ -3,7 +3,7 @@
     <div class="artist mb-3 col-md-3 col-6 col-sm-4">
         <a href="{{ route('artists.get-show', ['id' => $artist->id, 'slug' => $artist->url]) }}">
             <div style="" class="artist-img">
-                <img src="{{ $artist->poster }}" alt="{{ $artist->name }}" class="img-fit fimg">
+                <img src="{{ $artist->poster }}" alt="{{ $artist->name }}" class="img-fit">
             </div>
         </a>
         <div class="artist-title">
