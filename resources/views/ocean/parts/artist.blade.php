629b8130629387eaccf3a55adf0685530663cca1
Bakhodir <boxo2016@gmail.com>
2021-05-13 12:51:57 +0500

    bootstrap grid

--- resources/views/ocean/parts/artist.blade.php
+++ resources/views/ocean/parts/artist.blade.php
@@ -1,6 +1,6 @@
 <?php /** @var $artists \App\Models\Artist[] */ ?>
 @foreach($artists as $artist)
-    <div class="artist col-md-3 col-6 col-sm-4">
+    <div class="artist mb-3 col-md-3 col-6 col-sm-4">
         <a href="{{ route('artists.get-show', ['id' => $artist->id, 'slug' => $artist->url]) }}">
             <div style="" class="artist-img">
                 <img src="{{ $artist->poster }}" alt="{{ $artist->name }}" class="img-fit fimg">
