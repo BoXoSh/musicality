b26e1b0c6b1b61dfed80ed571a6cd42e143837a1
Bakhodir <boxo2016@gmail.com>
2021-05-13 11:57:35 +0500

    bootstrap grid

--- resources/views/ocean/parts/album.blade.php
+++ resources/views/ocean/parts/album.blade.php
@@ -1,10 +1,12 @@
 <?php /** @var $albums \App\Models\Album[] */ ?>
 @foreach($albums as $album)
-    <div class="album col-md-3">
+    <div class="album col-md-3 col-6 col-sm-4">
         <a href="{{ route('albums.get-show', ['id' => $album->id, 'slug' => $album->url]) }}">
             <img src="{{ $album->poster }}" alt="{{ $album->name }}" class="img-fit fimg">
         </a>
-        <p class="album-title"><a href="{{ route('albums.get-show', ['id' => $album->id, 'slug' => $album->url]) }}">{{ $album->name }}</a></p>
+        <p class="album-title"><a
+                    href="{{ route('albums.get-show', ['id' => $album->id, 'slug' => $album->url]) }}">{{ $album->name }}</a>
+        </p>
         <p class="album-artist">
             {{ $album->artist_name }}
         </p>
