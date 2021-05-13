eb47a30076f1a44715fa0e09a7b4e5e38dc577b4
Bakhodir <boxo2016@gmail.com>
2021-05-13 11:20:53 +0500

    albums

--- resources/views/ocean/parts/album.blade.php
+++ resources/views/ocean/parts/album.blade.php
@@ -3,7 +3,7 @@
     <div class="fimg img-fit">
         <a href="{{ route('albums.get-show', ['id' => $album->id, 'slug' => $album->url]) }}">
             <img src="{{ $album->poster }}" alt="{{ $album->name }}">
-            <p>{{ $album->name }}</p>
         </a>
+        <p><a href="{{ route('albums.get-show', ['id' => $album->id, 'slug' => $album->url]) }}">{{ $album->name }}</a></p>
     </div>
 @endforeach
