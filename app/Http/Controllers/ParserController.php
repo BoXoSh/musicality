aafb30c14fb5e359c8d7f897e65bc260b653f666
Bakhodir <boxo2016@gmail.com>
2021-05-23 00:30:12 +0500

    parser tojik

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -15,6 +15,107 @@ class ParserController extends Controller
 
     public $rootPath = '../admin.muz.asia/';
 
+    public function kzmp3()
+    {
+        $category_id = 8;
+        $urlProxy = 'https://parser.top/most.php?url=';
+        $dom = new Document($urlProxy . 'https://kzmp3.kz/music/kaz', true);
+        $items = $dom->first('.main__inner')->findInDocument('.top__item');
+
+        foreach ($items as $item) {
+            $song_title_el = $item->firstInDocument('.song_title');
+            $song_id = preg_replace('#^https://kzmp3.kz/([0-9]+)-.*.html$#si', '$1', $song_title_el->attr('href'));
+
+            if (empty($song_id))
+                continue;
+
+            $track_link = 'https://kzmp3.kz/api/mp3_download/' . $song_id;
+
+            $artist = $song_title_el->firstInDocument('.top__singer');
+            if (!$artist)
+                continue;
+            $artist = trim($artist->text());
+
+
+            $title = $song_title_el->firstInDocument('.top__name');
+            if (!$title)
+                continue;
+            $title = trim($title->text());
+
+            $img = 'https://kzmp3.kz/uploads/artist/' . $song_id . '.jpg';
+
+            $full_title = $artist . ' - ' . $title;
+            $track_time = $item->firstInDocument('.top__time');
+            $track_time = $track_time ? trim($track_time->text()) : '';
+
+            $artistModel = $this->createArtist($artist, $img);
+            $alt_name = Str::slug($full_title);
+
+            if (Post::query()->where('alt_name', $alt_name)->exists())
+                continue;
+
+            $track_path = 'uploads/files/' . Str::random() . '_' . $alt_name . '.mp3';
+
+            if (!$filesize = $this->saveFile($urlProxy . $track_link, $track_path))
+                continue;
+
+            $xfields = 'artist_name|' . $artist . '||track_name|' . $title . '||name|{name}||description|{description}||id_youtube_0|{id_youtube_0}||duration|' . $track_time . '||size|' . $this->formatSizeUnits($filesize) . '||year|2021';
+
+
+            if ($artistModel) {
+                $xfields .= '||artists_link|<a href=\"/artist/' . $artistModel->id . '-' . $artistModel->url . '.html\">' . $artist . '</a>||id_artist|' . $artistModel->id_zvuk;
+                if (!empty($img)) {
+                    $xfields .= '||poster|' . $artistModel->poster . '||image|' . $img;
+                }
+            }
+
+            if (!empty($track_path))
+                $xfields .= '||music_link|/' . $track_path;
+
+            $post = Post::create([
+                'autor' => 'Admin',
+                'date' => date('Y-m-d H:i:s'),
+                'short_story' => '',
+                'full_story' => '',
+                'xfields' => $xfields,
+                'title' => $full_title,
+                'descr' => '',
+                'keywords' => '',
+                'category' => $category_id,
+                'alt_name' => Str::slug($full_title),
+                'comm_num' => 0,
+                'allow_comm' => 1,
+                'allow_main' => 1,
+                'approve' => 1,
+                'fixed' => 0,
+                'allow_br' => 1,
+                'symbol' => '',
+                'tags' => $full_title,
+                'metatitle' => $full_title,
+                'zvuk_id' => 0,
+            ]);
+
+            if (!$post)
+                continue;
+
+
+            PostExtrasCats::create([
+                'news_id' => $post->id,
+                'cat_id' => $category_id
+            ]);
+
+            PostExtras::create([
+                'news_id' => $post->id,
+                'related_ids' => '',
+                'access' => '',
+                'editor' => '',
+                'reason' => '',
+                'user_id' => 1
+            ]);
+        }
+    }
+
+
     public function hittj()
     {
         $category_id = 9;
@@ -103,12 +204,13 @@ class ParserController extends Controller
         $slug = Str::slug($name);
         $artist = Artist::query()->where('url', $slug)->first();
 
-        $poster_url = '';
+        $poster_url = '/ocean/images/no_image.jpg';
         if (!$artist) {
             if (!empty($poster)) {
                 $posterPath = 'uploads/artist/' . date('Y-m') . '/' . $slug . '.jpeg';
                 if ($this->saveFile($poster, $posterPath))
                     $poster_url = '/' . $posterPath;
+
             }
             $artist = Artist::create([
                 'name' => $name,
